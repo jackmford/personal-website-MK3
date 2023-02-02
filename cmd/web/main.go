df4db93d1519c5a681db1b20ff6d929a9516b209
jackmford <jackmitchellfordyce@gmail.com>
2023-02-02 15:30:37 -0600

    http.Errors

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,14 +15,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles("./ui/html/pages/index.tmpl")
 	if err != nil {
 		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
 		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
